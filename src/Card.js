4ffbf3485f4f839ad441d6a95319b16b57c5db8e
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-01-05 19:20:13 +0200

    created card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img alt='robots' src=''/>
+            <div>
+                <h2>John Smith</h2>
+                <p>john.smith@gmail.com</p>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
