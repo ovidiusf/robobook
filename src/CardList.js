16a6e3211e9dd7f38383af4bfa72d00d1d50a2b3
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-01-05 21:37:40 +0200

    created CardList component which renders all cards from robots

---
+++ src/CardList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+    const cardsArray = robots.map((user, index) => {
+        return (<Card
+            key={robots[index].id}
+            id={robots[index].id}
+            name={robots[index].name}
+            email={robots[index].email} />
+        );
+    });
+    return (
+        <div>
+            {cardsArray}
+        </div>
+    );
+};
+
+
+export default CardList;
