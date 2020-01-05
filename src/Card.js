4a5bff1e921e14fefc5adeeaf03786efaf639d3a
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-01-05 21:07:30 +0200

    added styling for the Card components

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Card = () => {
     return (
-        <div>
-            <img alt='robots' src=''/>
+        <div className="bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt='robots' src='https://robohash.org/test?200x200'/>
             <div>
                 <h2>John Smith</h2>
                 <p>john.smith@gmail.com</p>
