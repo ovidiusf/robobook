10e52f5010dcb891d06ae80b33ebf682bf15210c
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-01-05 21:18:01 +0200

    added robots.js with robot names, props to the index.js file and props to the Card Component

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({id, name, email}) => {
     return (
-        <div className="bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img alt='robots' src='https://robohash.org/test?200x200'/>
+        <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt='robots' src={`https://robohash.org/t${id}?200x200`}/>
             <div>
-                <h2>John Smith</h2>
-                <p>john.smith@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
