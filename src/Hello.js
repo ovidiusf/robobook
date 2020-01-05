c96342abea5e3fa1d95c74aa7e26421c5ae1a694
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-01-05 19:13:52 +0200

    testing props and converting the class Hello to a function

--- src/Hello.js
+++ src/Hello.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import './Hello.css';
 
-class Hello extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Hello World!</h1>
-                <p>Welcome to React!</p>
-            </div>
-        );
-    }
+const Hello = (props) => {
+    return (
+        <div className='f1 tc'>
+            <h1>Hello World!</h1>
+            <p>{props.greeting}</p>
+        </div>
+    );
 }
+
 
 export default Hello;
 
--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 // import App from './App';
 import Hello from './Hello'
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<Hello greeting={'Hello, React User!'}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
