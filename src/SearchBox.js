752ad4be75bdc659816718f7104ad974852fef90
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-01-07 10:54:06 +0200

    added h1 title and searchbox

---
+++ src/SearchBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchBox = () => {
+    return (
+        <div>
+            <input 
+            type="search" 
+            size="35" 
+            placeholder="Search your robot friends..." />
+        </div>
+    );
+};
+
+export default SearchBox;
