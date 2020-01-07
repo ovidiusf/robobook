e24e4d660f7855572f127153a1cdeb1aa44765ee
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-01-07 11:40:16 +0200

    added searchbox functionality with state management

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
 
-const App = () => {
-    return (
-        <div>
-            <h1 className='tc'>RoboBook</h1>
-            <SearchBox />
-            <CardList robots={robots} />
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchField: ''
+        };
+    };
+
+    // use arrow functions for selfmade functions
+    //constructor and render come with react and can use the older syntax
+    // if using the same syntax, the this. will call the input field, not the App
+    // hence not allowing us to access this.state
+
+    // in order to update state, we always use this.setState
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value });
+    };
+
+    // Renders the app component; inside we have props for SearchBox and CardList; searchChange is a prop for SearchBox
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        });
+        return (
+            <div className="tc">
+                <h1>RoboBook</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} />
+            </div>
+        );
+    };
 };
+
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-const SearchBox = () => {
+// every time the on change event is triggered, call the onchange function
+// searchChange is the prop that is passed
+const SearchBox = ({searchChange}) => {
     return (
-        <div>
-            <input 
+        <div className="pa2">
+            <input className="pa3 ba b--blue bg-lightest-blue"
             type="search" 
             size="35" 
-            placeholder="Search your robot friends..." />
+            placeholder="Search your robot friends..." 
+            onChange={searchChange}/>
         </div>
     );
 };
