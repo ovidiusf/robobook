255b3e5c221705ca3b47bb9d72eed2bb8d5d6243
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-01-08 15:12:59 +0200

    Updates

---
+++ precache-manifest.4c93d53250e31072314ab7285cd61720.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3091478be6dd62a38ae0ba0aba3306be",
+    "url": "/robobook/index.html"
+  },
+  {
+    "revision": "7bc79035924b0ca7a01f",
+    "url": "/robobook/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "df025eb3ec3615b122cf",
+    "url": "/robobook/static/css/main.4260caf0.chunk.css"
+  },
+  {
+    "revision": "7bc79035924b0ca7a01f",
+    "url": "/robobook/static/js/2.e8ab5529.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robobook/static/js/2.e8ab5529.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df025eb3ec3615b122cf",
+    "url": "/robobook/static/js/main.542f3027.chunk.js"
+  },
+  {
+    "revision": "0d93c643bac7a417210a",
+    "url": "/robobook/static/js/runtime-main.3f7ea2c0.js"
+  },
+  {
+    "revision": "306bd2f495e27b09e2f0cb82f98b1a62",
+    "url": "/robobook/static/media/jagwi___-webfont.306bd2f4.woff2"
+  },
+  {
+    "revision": "9d0b58ba76b72130aa7773651649c9a8",
+    "url": "/robobook/static/media/jagwi___-webfont.9d0b58ba.woff"
+  }
+]);
