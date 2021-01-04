8a4a9631f041dcbb0defd647c2ad57a2073e7af3
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2021-01-04 11:49:17 -0800

    Bump react and dependencies version to latest

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,24 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class ErrorBoundary extends Component {
-    constructor(props) {
-       super(props);
-        this.state = {
-            hasError: false
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false
     };
+  }
 
-    componentDidCatch(error, info) {
-        this.setState({hasError: true});
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Oooops. Something went wrong!</h1>;
     }
-
-    render() {
-        if (this.state.hasError){
-            return <h1>Oooops. Something went wrong!</h1>
-        }
-        return this.props.children;
-    };
-};
+    return this.props.children;
+  }
+}
 
 export default ErrorBoundary;
-
--- src/containers/App.js
+++ src/containers/App.js
@@ -8,61 +8,58 @@ import './App.css';
 import api_url from '../API';
 import { setSearchField } from '../actions';
 
-const mapStateToProps = state => {
-    return {
-        searchField: state.searchField
-    };
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchField
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
-    };
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  };
 };
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: []
-        };
+  constructor() {
+    super();
+    this.state = {
+      robots: []
     };
+  }
 
-    async componentDidMount() {
-        // console.log(this.props.store.getState());
-        const response = await fetch(api_url);
-        const users = await response.json();
-        this.setState({ robots: users });
-    };
+  async componentDidMount() {
+    const response = await fetch(api_url);
+    const users = await response.json();
+    this.setState({ robots: users });
+  }
 
-    // use arrow functions for selfmade functions
-    //constructor and render come with react and can use the older syntax
-    // if using the same syntax, the this. will call the input field, not the App
-    // hence not allowing us to access this.state
+  // use arrow functions for selfmade functions
+  //constructor and render come with react and can use the older syntax
+  // if using the same syntax, the this. will call the input field, not the App
+  // hence not allowing us to access this.state
 
-    // Renders the app component; inside we have props for SearchBox and CardList; searchChange is a prop for SearchBox
-    render() {
-        const { robots } = this.state;
-        const { searchField, onSearchChange } = this.props;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchField.toLowerCase());
-        });
-        return !robots.length ?
-            <h2 className='loading'>Loading...</h2>
-            :
-            (
-                <div className='tc'>
-                    <h1 className='f1'>RoboBook</h1>
-                    <SearchBox searchChange={onSearchChange} />
-                    <Scroll>
-                        <ErrorBoundary>
-                            <CardList robots={filteredRobots} />
-                        </ErrorBoundary>
-                    </Scroll>
-                </div>
-            );
-
-    };
-};
+  // Renders the app component; inside we have props for SearchBox and CardList; searchChange is a prop for SearchBox
+  render() {
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+    return !robots.length ? (
+      <h2 className='loading'>Loading...</h2>
+    ) : (
+      <div className='tc'>
+        <h1 className='f1'>RoboBook</h1>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
+        </Scroll>
+      </div>
+    );
+  }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
